8a6a71d56f0d682769e97b6c88d177fb3f2b85cc
Dave Brosius <dbrosius@mebigfatguy.com>
2011-11-26 22:48:32 -0500

    add javadoc

--- prototype/com/mebigfatguy/caveman/proto/CMKeyCMValueMap.java
+++ prototype/com/mebigfatguy/caveman/proto/CMKeyCMValueMap.java
@@ -23,29 +23,156 @@ import com.mebigfatguy.caveman.proto.aux.CMValue;
 import com.mebigfatguy.caveman.proto.aux.CMValueBag;
 
 
+/**
+ * An object that maps <b>CMKey</b> keys to <b>CMValue</b> values.  A map cannot contain duplicate keys;
+ * each key can map to at most one value.
+ * 
+ * <p>The <tt>CMKeyCMValueMap</tt> interface provides two <i>collection views</i>, which
+ * allow a map's contents to be viewed as a set of keys pr collection of values
+ * The <i>order</i> of a map is defined as the order in which the iterators on the map's collection 
+ * views return their elements.  
+ */
+
 public interface CMKeyCMValueMap {
 	
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
 	int size();
 	
+    /**
+     * Returns <tt>true</tt> if this map contains no key-value mappings.
+     *
+     * @return <tt>true</tt> if this map contains no key-value mappings
+     */
 	boolean isEmpty();
 	
+    /**
+     * Returns <tt>true</tt> if this map contains a mapping for the specified
+     * <b>CMKey</b> key.
+     *
+     * @param key key whose presence in this map is to be tested
+     * @return <tt>true</tt> if this map contains a mapping for the specified
+     *         <b>CMKey</b>
+     */
 	boolean containsKey(CMKey key);
 	
+    /**
+     * Returns <tt>true</tt> if this map maps one or more <b>CMKey</b> keys to the
+     * specified <b>CMValue</b> value. This method is provided for completeness, 
+     * but will not perform efficiently for large maps.
+     *
+     * @param value value whose presence in this map is to be tested
+     * @return <tt>true</tt> if this map maps one or more keys to the
+     *         specified value
+     */
 	boolean containsValue(CMValue value);
 	
+    /**
+     * Returns the value to which the specified <b>CMKey</b> key is mapped,
+     * or a default value if this map contains no mapping for the key.
+     * The default value can be specified when constructing the map.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or
+     *         {@code null} if this map contains no mapping for the key
+     */
 	CMValue get(CMKey key);
 	
+    /**
+     * Associates the specified <b>CMValue</b> value with the specified <b>CMKey</b> key in this map
+     * (optional operation).  If the map previously contained a mapping for
+     * the key, the old value is replaced by the specified value.
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with <tt>key</tt>, or a 'not found' value if not found
+     * @throws UnsupportedOperationException if the <tt>put</tt> operation
+     *         is not supported by this map
+     */
 	void put(CMKey key, CMValue value);
 	
+    /**
+     * Removes the mapping for a <b>CMKey</b> key from this map if it is present
+     * (optional operation).
+     *
+     * <p>Returns the <b>CMValue</b> value to which this map previously associated the <b>CMKey</b> key,
+     * or <tt>null</tt> if the map contained no mapping for the key.
+     *
+     * <p>The map will not contain a mapping for the specified key once the
+     * call returns.
+     *
+     * @param key key whose mapping is to be removed from the map
+     * @return the previous value associated with <tt>key</tt>, or
+     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
+     *         is not supported by this map
+     */
 	void remove(CMKey key);
 	
+    /**
+     * Copies all of the mappings from the specified map to this map
+     * (optional operation).  The effect of this call is equivalent to that
+     * of calling {@link #put(Object,Object) put(k, v)} on this map once
+     * for each mapping from <b>CMKey</b> key <tt>k</tt> to <b>CMValue</b> value <tt>v</tt> in the
+     * specified map.  The behavior of this operation is undefined if the
+     * specified map is modified while the operation is in progress.
+     *
+     * @param m mappings to be stored in this map
+     * @throws UnsupportedOperationException if the <tt>putAll</tt> operation
+     *         is not supported by this map
+     */
 	void putAll(CMKeyCMValueMap m);
 	
+    /**
+     * Removes all of the mappings from this map (optional operation).
+     * The map will be empty after this call returns.
+     *
+     * @throws UnsupportedOperationException if the <tt>clear</tt> operation
+     *         is not supported by this map
+     */
 	void clear();
 	
+    /**
+     * Returns a view of the <b>CMKey</b> keys contained in this map.
+     * The set is backed by the map, so changes to the map are
+     * reflected in the set, and vice-versa.  If the map is modified
+     * while an iteration over the set is in progress (except through
+     * the iterator's own <tt>remove</tt> operation), the results of
+     * the iteration are undefined.  The set supports element removal,
+     * which removes the corresponding mapping from the map, via the
+     * <tt>Iterator.remove</tt>, <tt>Set.remove</tt>,
+     * <tt>removeAll</tt>, <tt>retainAll</tt>, and <tt>clear</tt>
+     * operations.  It does not support the <tt>add</tt> or <tt>addAll</tt>
+     * operations.
+     *
+     * @return a set view of the <b>CMKey</b> keys contained in this map
+     */
 	CMKeySet keySet();
 	
+    /**
+     * Returns a view of the <b>CMValue</b> values contained in this map.
+     * The collection is backed by the map, so changes to the map are
+     * reflected in the collection, and vice-versa.  If the map is
+     * modified while an iteration over the collection is in progress
+     * (except through the iterator's own <tt>remove</tt> operation),
+     * the results of the iteration are undefined.  The collection
+     * supports element removal, which removes the corresponding
+     * mapping from the map, via the <tt>Iterator.remove</tt>,
+     * <tt>Collection.remove</tt>, <tt>removeAll</tt>,
+     * <tt>retainAll</tt> and <tt>clear</tt> operations.  It does not
+     * support the <tt>add</tt> or <tt>addAll</tt> operations.
+     *
+     * @return a collection view of the <b>CMValue</b> values contained in this map
+     */
 	CMValueBag values();
 	
+	/**
+	 * Returns a iterator over the the <b>CMKey</b>, <b>CMValue</b> pairs in the map.
+	 * 
+	 * @return an iterator to navigate the map
+	 */
 	CMKeyCMValueMapIterator iterator();
 }
