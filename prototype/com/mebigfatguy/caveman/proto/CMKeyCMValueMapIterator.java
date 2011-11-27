3c2961cd77cfb955ba9115a7015333632871f4aa
Dave Brosius <dbrosius@mebigfatguy.com>
2011-11-26 23:18:22 -0500

    add javadoc

--- prototype/com/mebigfatguy/caveman/proto/CMKeyCMValueMapIterator.java
+++ prototype/com/mebigfatguy/caveman/proto/CMKeyCMValueMapIterator.java
@@ -17,20 +17,57 @@
  */
 package com.mebigfatguy.caveman.proto;
 
+import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
 
 import com.mebigfatguy.caveman.proto.aux.CMKey;
 import com.mebigfatguy.caveman.proto.aux.CMValue;
 
+/**
+ * An iterator over a CMKeyCMValueMap.
+ */
 public interface CMKeyCMValueMapIterator {
 
+    /**
+     * Returns <tt>true</tt> if the iteration has more <b>CMKey/CMValue</b> elements. (In other
+     * words, returns <tt>true</tt> if <tt>next</tt> would return an element
+     * rather than throwing an exception.)
+     *
+     * @return <tt>true</tt> if the iterator has more elements.
+     */
 	boolean hasNext();
 	
+    /**
+     * Progresses the iterator to the next <b>CMKey/CMValue</b> element in the iteration.
+     *
+     * @exception NoSuchElementException iteration has no more elements.
+     */
 	void next() throws NoSuchElementException;
 	
+	/**
+	 * Returns the <b>CMKey</b> key pointed at by the iterator
+	 * 
+	 * @return the key of the pair currently being pointed at by the iterator
+	 */
 	CMKey key();
 	
+	/**
+	 * Returns the <b>CMValue</b> value pointed at by the iterator
+	 * 
+	 * @return the value of the pair currently being pointed at by the iterator
+	 */
 	CMValue value();
 	
+    /**
+     * 
+     * Removes from the underlying collection the last <b>CMKey/CMValue</b> element returned by the
+     * iterator (optional operation).  This method can be called only once per
+     * call to <tt>next</tt>.  The behavior of an iterator is unspecified if
+     * the underlying collection is modified while the iteration is in
+     * progress in any way other than by calling this method.
+     
+     * @exception ConcurrentModificationException if the underlying collection has changed
+     * since the iterator was created.
+     */
 	void remove();
 }
--- prototype/com/mebigfatguy/caveman/proto/CMKeyMapIterator.java
+++ prototype/com/mebigfatguy/caveman/proto/CMKeyMapIterator.java
@@ -17,19 +17,56 @@
  */
 package com.mebigfatguy.caveman.proto;
 
+import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
 
 import com.mebigfatguy.caveman.proto.aux.CM;
 
+/**
+ * An iterator over a collection of <b>CM/Object</b> elements
+ */
 public interface CMKeyMapIterator<V> {
 	
+    /**
+     * Returns <tt>true</tt> if the iteration has more <b>CM/Object</b> elements. (In other
+     * words, returns <tt>true</tt> if <tt>next</tt> would return an element
+     * rather than throwing an exception.)
+     *
+     * @return <tt>true</tt> if the iterator has more elements.
+     */
 	boolean hasNext();
 	
+    /**
+     * Positions the iterator at the next <b>CM/Object</b> element in the iteration.
+     *
+     * @exception NoSuchElementException iteration has no more elements.
+     */
 	void next() throws NoSuchElementException;
 	
+	/**
+	 * Returns the <b>CM</b> key pointed at by the iterator
+	 * 
+	 * @return the key of the pair currently being pointed at by the iterator
+	 */
 	CM key();
 	
+	/**
+	 * Returns the <b>Object</b> value pointed at by the iterator
+	 * 
+	 * @return the value of the pair currently being pointed at by the iterator
+	 */
 	V value();
 	
+    /**
+     * 
+     * Removes from the underlying collection the last <b>CM/Object</b> element returned by the
+     * iterator (optional operation).  This method can be called only once per
+     * call to <tt>next</tt>.  The behavior of an iterator is unspecified if
+     * the underlying collection is modified while the iteration is in
+     * progress in any way other than by calling this method.
+     
+     * @exception ConcurrentModificationException if the underlying collection has changed
+     * since the iterator was created.
+     */
 	void remove();
 }
