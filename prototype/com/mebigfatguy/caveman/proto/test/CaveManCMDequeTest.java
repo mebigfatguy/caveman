3b541a269a129b05b5fd8482530f3946b36b1ad8
Dave Brosius <dbrosius@mebigfatguy.com>
2015-10-25 10:31:50 -0400

    add start of deque test

---
+++ prototype/com/mebigfatguy/caveman/proto/test/CaveManCMDequeTest.java
@@ -0,0 +1,49 @@
+/*
+ * caveman - A primitive collection library
+ * Copyright 2011-2015 MeBigFatGuy.com
+ * Copyright 2011-2015 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.caveman.proto.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mebigfatguy.caveman.proto.aux.CM;
+import com.mebigfatguy.caveman.proto.impl.CaveManCMDeque;
+
+public class CaveManCMDequeTest {
+    @Test
+    public void testSizeIsEmpty() {
+        CaveManCMDeque s = new CaveManCMDeque();
+        Assert.assertEquals(0, s.size());
+        Assert.assertTrue(s.isEmpty());
+        
+        for (int i = 0; i < 10; i++) {
+            s.add(toCaveMan(0));
+            s.add(toCaveMan(1));
+        }
+        
+        Assert.assertEquals(20, s.size());
+        Assert.assertFalse(s.isEmpty());
+        
+        s.remove(toCaveMan(0));
+        s.remove(toCaveMan(1));
+        
+        Assert.assertEquals(0, s.size());
+        Assert.assertTrue(s.isEmpty());
+    }
+    
+    private CM toCaveMan(int i) { return null; }
+}
